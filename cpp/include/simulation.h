2cb9a58c90a95349e201f63d86933d3dcc0a3887
MozhganKhanjanianpak <142083895+MozhganKhanjanianpak@users.noreply.github.com>
2026-07-13 13:39:49 +0330

    Create simulation.h

---
+++ cpp/include/simulation.h
@@ -0,0 +1,42 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include <vector>
+#include "network.h"
+#include "parameters.h"
+
+class Simulation
+{
+public:
+
+    Simulation();
+
+    void buildNetwork();
+
+    void initializeNetwork();
+
+    void updateLinks();
+
+    void computeInputs();
+
+    void updateNodes();
+
+    void writeOutput(int t);
+
+    void run();
+
+private:
+
+    // Network
+    std::vector<Link> adj[N];
+
+    // Neuron states
+    int node_state[N];
+    int node_input[N];
+
+    // Statistics
+    int total_link;
+
+};
+
+#endif
