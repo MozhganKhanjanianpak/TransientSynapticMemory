5e2412817f5843e5690876b8cda39e2e0ef32362
MozhganKhanjanianpak <142083895+MozhganKhanjanianpak@users.noreply.github.com>
2026-07-14 10:18:49 +0330

    Update main.cpp

--- cpp/examples/main.cpp
+++ cpp/examples/main.cpp
@@ -1,49 +1,19 @@
-#include <iostream>
-#include <fstream>
 #include <cstdlib>
 #include <ctime>
 
 #include "../include/simulation.h"
 
-using namespace std;
+int main()
+{
+    srand(time(nullptr));
 
-//--------------------------------------------------
-int main() {
+    Simulation simulation;
 
-	srand(time(NULL));
-	
-	Simulation simulation;
-	
-	simulation.buildNetwork();
+    simulation.buildNetwork();
 
-	simulation.initializeNetwork();
+    simulation.initializeNetwork();
 
-	simulation.run();
+    simulation.run();
 
-
-	//---------------------------------------------------
-	
-	ofstream output("ActivityInTime.txt");
-	ofstream output_active_nodes_at_t("ActiveNodesInTime.txt");
-
-	int N_E = N * E;
-	int t = 0;	
-
-
-	// Output at t=0 ----------------------------------
-	double active = 0;
-	for (int i = 0; i < N; i++) {
-		active += node_state[i];
-	}
-
-	output << "0\t" << active / N << "\t0" << "\t0" << endl; // time-Rho-PhiE-PhiI
-
-	output_active_nodes_at_t << "0\t";
-	for (int i=0 ; i<N ; i++)
-		if (node_state[i]==1)
-			output_active_nodes_at_t << i << "\t";
-	output_active_nodes_at_t << endl;
-
-
-	return 0;
+    return 0;
 }
