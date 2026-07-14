ac83093aa96cf6310e3fc3102be045b1c6f86b0d
MozhganKhanjanianpak <142083895+MozhganKhanjanianpak@users.noreply.github.com>
2026-07-14 10:50:27 +0330

    Update output.cpp

--- cpp/src/output.cpp
+++ cpp/src/output.cpp
@@ -1,15 +1,33 @@
 #include "output.h"
 
+#include <stdexcept>
+
+/////////////////////////////////////////////////////////////
+// Constructor
+/////////////////////////////////////////////////////////////
+
 Output::Output()
 {
     activityFile.open("ActivityInTime.txt");
 
+    if (!activityFile.is_open())
+        throw std::runtime_error("Cannot open ActivityInTime.txt");
+
     activeNodesFile.open("ActiveNodesInTime.txt");
+
+    if (!activeNodesFile.is_open())
+        throw std::runtime_error("Cannot open ActiveNodesInTime.txt");
 }
+
+/////////////////////////////////////////////////////////////
+// Destructor
+/////////////////////////////////////////////////////////////
 
 Output::~Output()
 {
-    activityFile.close();
+    if (activityFile.is_open())
+        activityFile.close();
 
-    activeNodesFile.close();
+    if (activeNodesFile.is_open())
+        activeNodesFile.close();
 }
